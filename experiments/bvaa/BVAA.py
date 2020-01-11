c00bb4dc148fd3ffe20a75eced543170ca9448e8
Ujjwal-9 <ujjwalupadhyay8@gmail.com>
2020-01-11 21:30:53 +0530

    fgsm attack on latent space

--- experiments/bvaa/BVAA.py
+++ experiments/bvaa/BVAA.py
@@ -444,11 +444,12 @@ def test(model, vae_model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            data_hat,_,_ = vae_model(torch.FloatTensor(data).to(device))
             data, target = data.to(device), target.to(device)
-            data_hat,_,_ = vae_model(data)
             output = model(data_hat)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
